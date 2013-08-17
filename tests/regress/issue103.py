2bfa7c78bbbcec0e7251602c6613269920763bfb
paul.betafive@gmail.com <paul.betafive@gmail.com@e8e0d7a0-c8d9-11dd-a880-a1081c7ac358>
2013-08-17 14:03:14 +0000

    tests/regress: Split issue 58
    issue58.py was failing but not for the reason described in issue 58 in the bug
    tracker. So the failing case was split off into a newly reported issue 103 with
    corresponding code in issue103.py.
    
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>
    
    
    
    git-svn-id: http://opkg.googlecode.com/svn/trunk@672 e8e0d7a0-c8d9-11dd-a880-a1081c7ac358

--- tests/regress/issue103.py
+++ tests/regress/issue103.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
 #
-# Reporter: Graham Gower
+# Reporter: Paul Barker
 #
 # What steps will reproduce the problem?
 # ======================================
 # 
-# 1. Create package A which recommends B.
+# 1. Create packages A and C which both recommend B.
 # 2. Create package B.
-# 3. Install A.
+# 3. Install A and C.
 # 4. Remove A with --autoremove 
 #
 #
 # What is the expected output? What do you see instead?
 # =====================================================
 # 
-# Expect B to be removed with A. Instead only A is removed.
+# Expect B to remain installed as it is still depended on by C. Instead, B is
+# removed.
+#
+# This test case was initially part of issue58.py but the failure was not as
+# described in issue 58 in the bug tracker, so this was split out.
 #
 #
 # Status
 # ======
 #
-# Fixed with r555/r556.
+# Open
 
 import os
 import opk, cfg, opkgcl
@@ -30,13 +34,16 @@ opk.regress_init()
 o = opk.OpkGroup()
 o.add(Package="a", Recommends="b")
 o.add(Package="b")
+o.add(Package="c", Recommends="b")
 o.write_opk()
 o.write_list()
 
 opkgcl.update()
 
 opkgcl.install("a")
+opkgcl.install("c")
 
 opkgcl.remove("a", "--autoremove")
-if opkgcl.is_installed("b"):
-	opk.fail("Recommended package 'b' not autoremoved.")
+if not opkgcl.is_installed("b"):
+	opk.fail("Pacakge 'b' removed despite remaining "
+			"recommending package 'c'.")
