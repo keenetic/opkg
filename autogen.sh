19f6ed7fdd0ed5ae8e4358c239ca3f29887fa6f7
paul.betafive@gmail.com <paul.betafive@gmail.com@e8e0d7a0-c8d9-11dd-a880-a1081c7ac358>
2013-08-29 20:36:13 +0000

    autogen.sh: Add '--clean' option
    Running './autogen --clean' should now remove all files that are autogenerated
    and not included in the checkout from version control.
    
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>
    
    git-svn-id: http://opkg.googlecode.com/svn/trunk@687 e8e0d7a0-c8d9-11dd-a880-a1081c7ac358

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,38 @@
 #! /bin/sh
+
+set -e
+
+if [ \( $# -eq 1 \) -a \( "$1" = "--clean" \) ]; then
+	# Deep clean of all generated files
+	rm -f configure
+	rm -f config.log config.status
+	rm -f libtool
+	rm -f aclocal.m4
+	rm -f libopkg.pc
+	rm -f man/{opkg-cl.1,opkg-key.1}
+	rm -f Makefile {libbb,libopkg,tests,man,utils,src}/Makefile
+	rm -f Makefile.in {libbb,libopkg,tests,man,utils,src}/Makefile.in
+	rm -f libopkg/config.h{,.in}
+	rm -f shave/{ltoptions.m4,ltsugar.m4,ltversion.m4,lt~obsolete.m4}
+	rm -f shave/{libtool.m4,shave,shave-libtool}
+	rm -rf po conf autom4te.cache
+	rm -rf {libbb,libopkg,src,tests}/.deps
+
+	rm -f {libbb,libopkg,tests,src}/*.o
+	rm -f {libbb,libopkg}/*.lo
+	rm -f {libbb,libopkg}/*.la
+	rm -f src/opkg-cl tests/libopkg_test
+	rm -f libopkg/stamp-h1
+	rm -f utils/update-alternatives
+	rm -rf {libbb,libopkg,tests,src}/.libs
+
+	rm -f tests/regress/*.py{c,o}
+	rm -rf tests/regress/__pycache__
+
+	exit 0
+fi
+
+# If we didn't get '--clean' we're bootstrapping the project
 autoreconf -v --install || exit 1
 glib-gettextize --force --copy || exit 1
 ./configure "$@"
