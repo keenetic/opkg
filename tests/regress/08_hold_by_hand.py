f782d7c93c836201df7aa989b6575c3cd2af6945
paul.betafive@gmail.com <paul.betafive@gmail.com@e8e0d7a0-c8d9-11dd-a880-a1081c7ac358>
2013-10-03 20:04:21 +0000

    tests/regress: Check hold flag works as expected
    Add two new tests which confirm the operation in different cases.
    
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>
    
    git-svn-id: http://opkg.googlecode.com/svn/trunk@714 e8e0d7a0-c8d9-11dd-a880-a1081c7ac358

---
+++ tests/regress/08_hold_by_hand.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+#
+# Within the function `pkg_hash_fetch_best_installation_candidate`, we wish to
+# test the condition where `(held_pkg && good_pkg_by_name) == true`. So install
+# a 1.0, mark it as on hold and explicitly install 'a_2.0_all.opk'. This
+# installation should be blocked.
+#
+
+import os
+import opk, cfg, opkgcl
+
+opk.regress_init()
+
+a_10 = opk.Opk(Package="a", Version="1.0")
+a_10.write()
+a_20 = opk.Opk(Package="a", Version="2.0")
+a_20.write()
+
+opkgcl.install("a_1.0_all.opk")
+if not opkgcl.is_installed("a"):
+	opk.fail("Package 'a' installed but reports as not installed.")
+
+opkgcl.flag_hold("a")
+
+# Try to explicitly install a version 2.0
+opkgcl.install("a_2.0_all.opk")
+
+if opkgcl.is_installed("a", "2.0"):
+	opk.xfail("Old version of package 'a' flagged as on hold but was upgraded.")
+if not opkgcl.is_installed("a", "1.0"):
+	opk.fail("Package 'a' not upgraded but old version was removed.")
---
+++ tests/regress/09_hold_depends.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+#
+# Mark an installed package a-1.0 as on hold, make available a-2.0 depended on
+# by z. Check that z refuses to install due to a-1.0 being held.
+
+import os
+import opk, cfg, opkgcl
+
+opk.regress_init()
+
+o = opk.OpkGroup()
+o.add(Package="x", Version="1.0")
+o.write_opk()
+o.write_list()
+
+opkgcl.update()
+
+opkgcl.install("x")
+if not opkgcl.is_installed("x"):
+	opk.fail("Package 'x' installed but reports as not installed.")
+
+opkgcl.flag_hold("x")
+
+# Make new versions of 'x' and 'z' available
+o.add(Package="x", Version="2.0")
+o.add(Package="z", Depends="x>=2.0")
+o.write_opk()
+o.write_list()
+
+opkgcl.update()
+opkgcl.install("z")
+
+if opkgcl.is_installed("z"):
+	opk.fail("Package 'z' installed despite dependency on 'x>=2.0' with 'x' version 1.0 on hold.")
+if opkgcl.is_installed("x", "2.0"):
+	opk.fail("Old version of package 'x' flagged as on hold but was upgraded.")
+if not opkgcl.is_installed("x", "1.0"):
+	opk.fail("Package 'x' not upgraded but old version was removed.")
