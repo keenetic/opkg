edca497a481f71974066385d8473756beeffe385
paul.betafive@gmail.com <paul.betafive@gmail.com@e8e0d7a0-c8d9-11dd-a880-a1081c7ac358>
2013-10-28 13:45:37 +0000

    tests/regress: Test non-exclusive virtual package
    Ensure that we can install multiple providers of a virtual package as long as
    none of them CONFLICT.
    
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>
    
    git-svn-id: http://opkg.googlecode.com/svn/trunk@736 e8e0d7a0-c8d9-11dd-a880-a1081c7ac358

---
+++ tests/regress/12_virtual_no_conflicts.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+#
+# Install a package 'x' which PROVIDES 'v' and ensure that we can install
+# another package 'y' which also PROVIDES 'v'.
+
+import os
+import opk, cfg, opkgcl
+
+opk.regress_init()
+
+o = opk.OpkGroup()
+o.add(Package="x", Provides="v")
+o.add(Package="y", Provides="v")
+o.write_opk()
+o.write_list()
+
+opkgcl.update()
+
+opkgcl.install("x")
+if not opkgcl.is_installed("x"):
+	opk.fail("Package 'x' installed but reports as not installed.")
+
+# Now try to install "y", which should succeed
+
+opkgcl.install("y")
+if not opkgcl.is_installed("y"):
+	opk.xfail("Package 'y' not installed despite lack of conflicts.")
