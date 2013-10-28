99197b1e3bd14cc19bd21132527e8674b644bcbf
paul.betafive@gmail.com <paul.betafive@gmail.com@e8e0d7a0-c8d9-11dd-a880-a1081c7ac358>
2013-10-28 13:45:31 +0000

    tests/regress: Added tests for virtual packages
    Debian documentations says that if a package PROVIDES and CONFLICTS another
    package, it indicates that the provided package is virtual but no other provider
    of that virtual package should be installed at the same time. If we attempt to
    install two providers of a virtual package 'v', one of which CONFLICTS 'v', it
    should not matter which order we try to install them in, the second one should
    be blocked. With the patches recently submitted by Andreas Oberritter this works
    as expected if first package CONFLICTS 'v'. The current code doesn't work if the
    second package CONFLICTS 'v' but the first does not.
    
    Tests are added for both cases, with the latter case being expected to fail.
    
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>
    
    git-svn-id: http://opkg.googlecode.com/svn/trunk@735 e8e0d7a0-c8d9-11dd-a880-a1081c7ac358

---
+++ tests/regress/10_virtual_conflicts_first.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+#
+# Install a package 'x' which PROVIDES and CONFLICTS 'v', indicating that 'v' is
+# a virtual package and no other provider of this virtual package should be
+# installed whilst 'x' is installed. Then try to install 'y' which also PROVIDES
+# 'v'.
+
+import os
+import opk, cfg, opkgcl
+
+opk.regress_init()
+
+o = opk.OpkGroup()
+o.add(Package="x", Provides="v", Conflicts="v")
+o.add(Package="y", Provides="v")
+o.write_opk()
+o.write_list()
+
+opkgcl.update()
+
+opkgcl.install("x")
+if not opkgcl.is_installed("x"):
+	opk.fail("Package 'x' installed but reports as not installed.")
+
+# Now try to install "y", which should fail
+
+opkgcl.install("y")
+if opkgcl.is_installed("y"):
+	opk.fail("Package 'y' installed despite conflict with 'v' provided by 'x'.")
---
+++ tests/regress/11_virtual_conflicts_second.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+#
+# Install a package 'x' which PROVIDES 'v'. Then try to install 'y' which
+# PROVIDES and CONFLICTS 'v', indicating that no other provider of the virtual
+# package 'v' should be installed at the same time as 'y'.
+
+import os
+import opk, cfg, opkgcl
+
+opk.regress_init()
+
+o = opk.OpkGroup()
+o.add(Package="x", Provides="v")
+o.add(Package="y", Provides="v", Conflicts="v")
+o.write_opk()
+o.write_list()
+
+opkgcl.update()
+
+opkgcl.install("x")
+if not opkgcl.is_installed("x"):
+	opk.fail("Package 'x' installed but reports as not installed.")
+
+# Now try to install "y", which should fail
+
+opkgcl.install("y")
+if opkgcl.is_installed("y"):
+	opk.xfail("Package 'y' installed despite conflict with 'v' provided by 'x'.")
