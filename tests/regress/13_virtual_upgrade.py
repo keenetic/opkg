5a214ed8a9fd657fa114320db816731982081ce7
paul.betafive@gmail.com <paul.betafive@gmail.com@e8e0d7a0-c8d9-11dd-a880-a1081c7ac358>
2013-10-28 13:45:42 +0000

    tests/regress: Test virtual upgrade
    Check that we can still upgrade a package 'x' if it both PROVIDES and CONFLICTS
    a virtual package 'v'.
    
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>
    
    git-svn-id: http://opkg.googlecode.com/svn/trunk@737 e8e0d7a0-c8d9-11dd-a880-a1081c7ac358

---
+++ tests/regress/13_virtual_upgrade.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+#
+# Install version 1.0 of a package 'x' which PROVIDES and CONFLICTS 'v', then
+# try to upgrade it.
+
+import os
+import opk, cfg, opkgcl
+
+opk.regress_init()
+
+o = opk.OpkGroup()
+o.add(Package="x", Provides="v", Conflicts="v", Version="1.0")
+o.write_opk()
+o.write_list()
+
+opkgcl.update()
+
+opkgcl.install("x")
+if not opkgcl.is_installed("x"):
+	opk.fail("Package 'x' installed but reports as not installed.")
+
+# Now try to upgrade
+
+o.add(Package="x", Provides="v", Conflicts="v", Version="2.0")
+o.write_opk()
+o.write_list()
+
+opkgcl.update()
+opkgcl.upgrade()
+
+if not opkgcl.is_installed("x", "2.0"):
+	opk.fail("New version of package 'x' available during upgrade but was not installed")
+if opkgcl.is_installed("x", "1.0"):
+	opk.fail("Package 'a' upgraded but old version still installed.")
