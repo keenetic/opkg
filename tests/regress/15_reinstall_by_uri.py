5bbe5194075c97aaf9acd703f7840b26de25f556
Paul Barker <paul@paulbarker.me.uk>
2014-02-10 11:08:36 +0000

    tests/regress: Added test for force reinstall by URI
    This test ensures that a package is properly reinstalled when the initial
    version is installed by name and then a modified package is forced to be
    reinstalled by explicit URI.
    
    This test is currently expected to fail.
    
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>

---
+++ tests/regress/15_reinstall_by_uri.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+#
+#
+
+import os
+import opk, cfg, opkgcl
+
+opk.regress_init()
+
+o = opk.OpkGroup()
+
+# Create a 1.0 containing file "test1" and install by name
+f = open("test1", "w")
+f.write("Test")
+f.close()
+pkg = opk.Opk(Package="a", Version="1.0")
+pkg.write(data_files=["test1"])
+o.addOpk(pkg)
+o.write_list()
+os.unlink("test1")
+
+opkgcl.update()
+
+opkgcl.install("a")
+if not opkgcl.is_installed("a"):
+	opk.fail("Package 'a' installed but reports as not installed.")
+if not os.path.exists("%s/test1" % cfg.offline_root):
+	opk.fail("Package 'a' installed but file 'test1' not created.")
+
+# Destroy the old package
+os.unlink("a_1.0_all.opk")
+
+# Create a 2.0 containing file "test2" and upgrade by URI
+f = open("test2", "w")
+f.write("Test")
+f.close()
+pkg = opk.Opk(Package="a", Version="1.0")
+pkg.write(data_files=["test2"])
+o.addOpk(pkg)
+o.write_list()
+os.unlink("test2")
+
+opkgcl.install("a_1.0_all.opk", "--force-reinstall")
+
+if not os.path.exists("%s/test2" % cfg.offline_root):
+	opk.fail("Package 'a' force reinstalled but new file 'test2' not created.")
+if os.path.exists("%s/test1" % cfg.offline_root):
+	opk.xfail("Package 'a' force reinstalled but old file 'test1' not removed.")
