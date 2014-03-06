b9c89e19fac55df7c12fc5d41f67cf510156d6c0
Paul Barker <paul@paulbarker.me.uk>
2014-03-06 16:06:59 +0000

    opkg_gpg: Isolate gpg signature verification
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>

--- libopkg/opkg_download.c
+++ libopkg/opkg_download.c
@@ -40,8 +40,8 @@
 #include <curl/curl.h>
 #endif
 
-#if defined(HAVE_GPGME)
-#include <gpgme.h>
+#ifdef HAVE_GPGME
+#include "opkg_gpg.h"
 #endif
 
 #ifdef HAVE_OPENSSL
@@ -765,105 +765,7 @@ int
 opkg_verify_file (char *text_file, char *sig_file)
 {
 #if defined HAVE_GPGME
-    int status = -1;
-    int ret = -1;
-    gpgme_ctx_t ctx;
-    int have_ctx = 0;
-    gpgme_data_t sig, text, key;
-    int have_sig = 0, have_text = 0, have_key = 0;
-    gpgme_error_t err;
-    gpgme_verify_result_t result;
-    gpgme_signature_t s;
-    gpgme_protocol_t protocol = GPGME_PROTOCOL_OpenPGP;
-    char *trusted_path = NULL;
-
-    if (opkg_config->check_signature == 0 )
-	return 0;
-
-    gpgme_check_version (NULL);
-
-    err = gpgme_new (&ctx);
-    if (err) {
-	opkg_msg(ERROR, "Unable to create gpgme context: %s\n", gpg_strerror(err));
-	goto out_err;
-    }
-    have_ctx = 1;
-
-    err = gpgme_set_protocol (ctx, protocol);
-    if (err) {
-	opkg_msg(ERROR, "Unable to set gpgme protocol to OpenPGP: %s\n", gpg_strerror(err));
-	goto out_err;
-    }
-
-    trusted_path = root_filename_alloc("/etc/opkg/trusted.gpg");
-    if (!trusted_path) {
-	opkg_msg(ERROR, "Out of memory!\n");
-	goto out_err;
-    }
-
-    err = gpgme_data_new_from_file (&key, trusted_path, 1);
-    if (err) {
-	opkg_msg(ERROR, "Unable to get data from file %s: %s\n", trusted_path, gpg_strerror(err));
-	goto out_err;
-    }
-    have_key = 1;
-
-    err = gpgme_op_import (ctx, key);
-    if (err) {
-	opkg_msg(ERROR, "Unable to import key from file %s: %s\n", trusted_path, gpg_strerror(err));
-	goto out_err;
-    }
-
-    err = gpgme_data_new_from_file (&sig, sig_file, 1);
-    if (err) {
-	opkg_msg(ERROR, "Unable to get data from file %s: %s\n", sig_file, gpg_strerror(err));
-	goto out_err;
-    }
-    have_sig = 1;
-
-    err = gpgme_data_new_from_file (&text, text_file, 1);
-    if (err) {
-	opkg_msg(ERROR, "Unable to get data from file %s: %s\n", text_file, gpg_strerror(err));
-	goto out_err;
-    }
-    have_text = 1;
-
-    err = gpgme_op_verify (ctx, sig, text, NULL);
-    if (err) {
-	opkg_msg(ERROR, "Unable to verify signature: %s\n", gpg_strerror(err));
-	goto out_err;
-    }
-
-    result = gpgme_op_verify_result (ctx);
-    if (!result) {
-	opkg_msg(ERROR, "Unable to get verification data: %s\n", gpg_strerror(err));
-	goto out_err;
-    }
-
-    /* see if any of the signitures matched */
-    s = result->signatures;
-    while (s) {
-	status = gpg_err_code (s->status);
-	if (status == GPG_ERR_NO_ERROR) {
-	    ret = 0;
-	    break;
-	}
-	s = s->next;
-    }
-
-out_err:
-    if (have_sig)
-	gpgme_data_release (sig);
-    if (have_text)
-	gpgme_data_release (text);
-    if (have_key)
-	gpgme_data_release (key);
-    if (trusted_path)
-	free (trusted_path);
-    if (have_ctx)
-	gpgme_release (ctx);
-
-    return ret;
+    return opkg_verify_gpg_signature(text_file, sig_file);
 #elif defined HAVE_OPENSSL
     return opkg_verify_openssl_signature(text_file, sig_file);
 #else
