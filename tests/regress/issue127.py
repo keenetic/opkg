5079a908b09790f9fde0a450e6c2d67996f90c9d
Paul Barker <paul@paulbarker.me.uk>
2014-04-22 12:32:34 +0100

    tests/regress: Add test for issue 127
    This issue is now solved so a test is added to make sure it doesn't reappear.
    
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>

---
+++ tests/regress/issue127.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+#
+# Issue 127 reported by muusclaus
+#
+# 1. call "opkg-cl upgrade PACKAGE_NAME"
+# 2. PACKAGE_NAME is a not installed package
+#
+# What is the expected output?
+# I expected to get an error message, that the package is not installed yet.
+#
+# What do you see instead?
+# The package will be installed like as I do an "opkg-cl install ..."
+#
+
+import opk, cfg, opkgcl
+
+opk.regress_init()
+
+o = opk.OpkGroup()
+o.add(Package="a")
+o.write_opk()
+o.write_list()
+
+opkgcl.update()
+
+opkgcl.upgrade("a")
+if opkgcl.is_installed("a"):
+	opk.fail("Package 'a' installed by upgrade.")
