949c02c4250d63bbbb367636842fecd8b45cb986
Paul Barker <paul@paulbarker.me.uk>
2014-06-06 12:51:31 +0000

    opkg_conf: Fix default cache_dir
    The default cache directory should be under /var/cache, not /var/lib.
    
    This fixes issue 131.
    
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>

--- libopkg/opkg_conf.h
+++ libopkg/opkg_conf.h
@@ -35,9 +35,9 @@ extern "C" {
 #define OPKG_CONF_TMP_DIR_SUFFIX "opkg-XXXXXX"
 
 #define OPKG_CONF_DEFAULT_LISTS_DIR     OPKG_STATE_DIR_PREFIX "/lists"
-#define OPKG_CONF_DEFAULT_CACHE_DIR     OPKG_STATE_DIR_PREFIX "/cache"
 #define OPKG_CONF_DEFAULT_INFO_DIR      OPKG_STATE_DIR_PREFIX "/info"
 #define OPKG_CONF_DEFAULT_STATUS_FILE   OPKG_STATE_DIR_PREFIX "/status"
+#define OPKG_CONF_DEFAULT_CACHE_DIR     "/var/cache/opkg"
 #define OPKG_CONF_DEFAULT_CONF_FILE_DIR "/etc/opkg"
 #define OPKG_CONF_DEFAULT_LOCK_FILE     "/var/run/opkg.lock"
 
