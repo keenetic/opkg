9f3fd9771461b8a3b537b6ea47d324f1f680f9e7
Andreas Oberritter <obi@opendreambox.org>
2014-08-17 03:16:54 +0200

    tests/regress: Add failing test for upgrading auto-installed packages
    If upgrading with the --autoremove option enabled, an auto-
    installed package which has no more recommendations should
    get uninstalled instead of upgraded.
    
    Signed-off-by: Andreas Oberritter <obi@opendreambox.org>
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>

---
+++ tests/regress/18_upgrade_recommends.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+#
+# Install a package B, which recommends package A. Verify that A installs
+# automatically and gets flagged as auto-installed. Make a newer version
+# of A and B available. Do upgrade with --autoremove option and ensure
+# that A gets uninstalled correctly instead of being upgraded.
+#
+
+import os
+import opk, cfg, opkgcl
+
+opk.regress_init()
+
+o = opk.OpkGroup()
+o.add(Package="a", Version="1.0")
+o.add(Package="b", Version="1.0", Recommends="a")
+o.write_opk()
+o.write_list()
+
+opkgcl.update()
+
+opkgcl.install("b")
+if not opkgcl.is_installed("a"):
+	opk.fail("Package a should have been auto-installed")
+if not opkgcl.is_autoinstalled("a"):
+	opk.fail("Package a should have been marked as auto-installed")
+
+o = opk.OpkGroup()
+o.add(Package="a", Version="2.0")
+o.add(Package="b", Version="2.0")
+o.write_opk()
+o.write_list()
+
+opkgcl.update()
+opkgcl.upgrade("--autoremove")
+
+if opkgcl.is_installed("a"):
+	opk.xfail("Package a should have been auto-removed")
