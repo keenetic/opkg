e5167e39b31fde098a5d0436202cf67146b00da5
Paul Barker <paul@paulbarker.me.uk>
2014-11-13 12:30:42 +0000

    tests/regress: Add test case for issue 152
    In the case where multiple packages provide the same abstract package, but there
    is nothing to differentiate between the providers (neither is held, preferred,
    matches the name of the abstract package or is already installed),
    pkg_hash_fetch_best_installation_candidate errors out instead of picking one of
    the available providers. This is a vestige of an old code path where opkg
    expected the user to explicitly select a provider in this case. Instead we
    should now just pick any valid provider.
    
    This bug was found by the OpenEmbedded/Yocto Project autobuilder when testing
    opkg v0.2.3. Both 'libperl5' and 'perl' provided 'perl-module-config', but
    rootfs creation was failing and opkg said it couldn't find a provider of
    'perl-module-config'.
    
    This test case is currently expected to fail. A fix is known and will be merged
    shortly.
    
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>

---
+++ tests/regress/issue152.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+#
+# Issue 152
+#
+# Create a package 'a' which depends on 'p', where 'p' is provided by packages
+# 'b' and 'c'. Ensure that 'a' can be installed and either 'b' or 'c' is
+# installed.
+#
+# This ensures that opkg can select a provider when both providers are
+# effectively equal. Neither is preferred, neither is held, neither matches the
+# abstract package name, etc.
+#
+# In opkg v0.2.3 this failed: neither provider was selected and so no provider
+# of 'p' was returned.
+#
+
+import os
+import opk, cfg, opkgcl
+
+opk.regress_init()
+
+o = opk.OpkGroup()
+o.add(Package="a", Depends="p")
+o.add(Package="b", Provides="p")
+o.add(Package="c", Provides="p")
+o.write_opk()
+o.write_list()
+
+opkgcl.update()
+
+opkgcl.install("a")
+if not opkgcl.is_installed("a"):
+    opk.xfail("Package 'a' failed to install.")
+if not (opkgcl.is_installed("b") or opkgcl.is_installed("c")):
+    opk.fail("No provider of 'p' was installed.")
