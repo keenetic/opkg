a9e371eb798ee0ef399daa9cca3a2d6c9f5d63a2
Eric Yu <eric.yu@ni.com>
2015-08-03 17:57:50 -0500

    tests/regress: Add test for issue 162
    Tests for the case where attempting to install a package which has
    both conflicting and non-conflicting dependencies will result in
    the installation of the non-conflicting dependencies while leaving
    the conflicting dependencies and initial package uninstalled.
    
    Signed-off-by: Eric Yu <eric.yu@ni.com>
    Acked-by: Alejandro del Castillo <alejandro.delcastillo@ni.com>

---
+++ tests/regress/issue162.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+#
+# Reporter: Eric Yu
+#
+# What steps will reproduce the problem?
+# ======================================
+#
+# 1. Install a package 'x'
+# 2. Install a package 'a' which depends on 'b' and 'c', where 'c' conflicts with 'x'
+#
+# What is the expected output? What do you see instead?
+# =====================================================
+#
+# Expected:
+# 'x' remains installed, and 'a', 'b', and 'c' remain uninstalled because
+# 'c' conflicts with 'x'
+#
+# Actual:
+# 'x' remains installed. 'a' and 'c' are not installed, but 'b' is installed.
+#
+# Status
+# ======
+# Open
+#
+
+import os
+import opk, cfg, opkgcl
+
+opk.regress_init()
+
+o = opk.OpkGroup()
+o.add(Package="x")
+o.add(Package="a", Depends="b, c")
+o.add(Package="b")
+o.add(Package="c", Conflicts="x");
+
+
+o.write_opk()
+o.write_list()
+opkgcl.update()
+
+opkgcl.install("x")
+if not opkgcl.is_installed("x"):
+    opk.fail("Package 'x' installed but reports as not installed.")
+
+opkgcl.install("a")
+if opkgcl.is_installed("c"):
+    opk.fail("Package 'c' installed but conflicts with x.")
+if opkgcl.is_installed("a"):
+    opk.fail("Package 'a' installed but dependency 'c' is not installed due to a conflict.")
+if opkgcl.is_installed("b"):
+    opk.xfail("Package 'b' installed and orphaned.")
+
+opkgcl.remove("x")
+opkgcl.remove("a")
+opkgcl.remove("b")
+opkgcl.remove("c")
