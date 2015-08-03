d7ce0a1c7373eb11c26dc907c25f0f4cf7b1de8b
Eric Yu <eric.yu@ni.com>
2015-08-03 17:57:51 -0500

    tests/regress: Add test for issue 163
    Add test for a problem with --force-removal-of-dependent-packages
    where it does not properly remove packages depending on the removed
    package.
    
    Signed-off-by: Eric Yu <eric.yu@ni.com>
    Acked-by: Alejandro del Castillo <alejandro.delcastillo@ni.com>

---
+++ tests/regress/issue163.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python
+#
+# Reporter: Eric Yu
+#
+# What steps will reproduce the problem?
+# ======================================
+#
+# 1. Create packages 'a' and 'b', where 'a' depends on 'b'.
+# 2. Install 'a' (which also installs 'b')
+# 3. Remove 'b' with --force-removal-of-dependent-packages
+#
+# What is the expected output? What do you see instead?
+# =====================================================
+#
+# Removing 'b' with --force-removal-of-dependent-packages should remove
+# both 'b' and 'a'.
+#
+# Instead, only 'b' is removed, leaving 'a' installed.
+#
+# Status
+# ======
+# Open
+#
+
+import os
+import opk, cfg, opkgcl
+
+opk.regress_init()
+
+o = opk.OpkGroup()
+o.add(Package="a", Depends="b")
+o.add(Package="b")
+
+o.write_opk()
+o.write_list()
+
+opkgcl.update()
+
+opkgcl.install("a")
+if not opkgcl.is_installed("a"):
+        opk.fail("Package 'a' installed but reports as not installed")
+if not opkgcl.is_installed("b"):
+        opk.fail("Package 'b' should be installed as a dependency of 'a' "
+                 "but reports as not installed")
+
+opkgcl.remove("b","--force-removal-of-dependent-packages")
+if opkgcl.is_installed("b"):
+        opk.fail("Package 'b' removed but reports as installed.")
+if opkgcl.is_installed("a"):
+        opk.xfail("Package 'b' removed with --force-removal-of-dependent-packages "
+                  "but 'a' which depends on 'b' still installed.")
