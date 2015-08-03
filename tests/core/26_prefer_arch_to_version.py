b47484aa4870f450dd857496d4857bfa504988dd
Eric Yu <eric.yu@ni.com>
2015-08-03 17:57:49 -0500

    tests/core: Add test for --prefer-arch-to-version
    Add test to check that --prefer-arch-to-version causes architecture
    priority to be used over package version when determining package
    priority.
    
    Signed-off-by: Eric Yu <eric.yu@ni.com>
    Acked-by: Alejandro del Castillo <alejandro.delcastillo@ni.com>

---
+++ tests/core/26_prefer_arch_to_version.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+#
+# Create package 'x(1.0)' with architecture a and 'x(2.0)' with architecture b.
+# Install 'x' with --add-arch a:2 and --add-arch b:1.
+# Check that 'x(2.0)' is installed. Then remove 'x'.
+# Install 'x' with --add-arch a:2 and --add-arch b:1 and --prefer-arch-to-version.
+# Check that 'x(1.0)' is installed.
+#
+
+import os
+import opk, cfg, opkgcl
+
+opk.regress_init()
+
+o = opk.OpkGroup()
+o.add(Package="x", Version="1.0", Architecture="a")
+o.add(Package="x", Version="2.0", Architecture="b")
+
+o.write_opk()
+o.write_list()
+
+opkgcl.update()
+
+arch_flags = "--add-arch a:2 --add-arch b:1"
+
+# install should prioritize version
+opkgcl.install("x", arch_flags)
+if opkgcl.is_installed("x", "1.0", arch_flags):
+        opk.fail("Package 'x(2.0)' available but 1.0 installed")
+if not opkgcl.is_installed("x", "2.0", arch_flags):
+        opk.fail("Package 'x(2.0)' available but was not installed.")
+
+opkgcl.remove("x", arch_flags)
+if opkgcl.is_installed("x", "2.0", arch_flags):
+        opk.fail("Package 'x' removed but reports as installed.")
+
+# prefer-arch-to-version should prefer the architecture
+opkgcl.install("x", arch_flags + " --prefer-arch-to-version")
+if opkgcl.is_installed("x", "2.0", arch_flags):
+    opk.fail("Package 'x(1.0) has preferred arch but 2.0 was installed")
+if not opkgcl.is_installed("x", "1.0", arch_flags):
+    opk.fail("Package 'x(1.0)' with preferred arch available but was not installed.")
