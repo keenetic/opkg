b0a8fd01f9bc109713701b4fd7a191b0da742b0f
Eric Yu <eric.yu@ni.com>
2015-08-03 17:57:52 -0500

    tests/regress: Add tests for issue 165
    Add tests for problems involving the installation of packages that
    recommend other packages that can't be installed due to conflicts.
    
    Signed-off-by: Eric Yu <eric.yu@ni.com>
    Acked-by: Alejandro del Castillo <alejandro.delcastillo@ni.com>

---
+++ tests/regress/issue165.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+#
+# Reporter: Eric Yu
+#
+# What steps will reproduce the problem?
+# ======================================
+#
+# 1. Create packages 'x', 'a', and 'b' such that 'a' recommends 'b'
+#    and 'b' conflicts 'x'.
+# 2. Install 'x'.
+# 3. Try to install 'a'.
+#
+# What is the expected output? What do you see instead?
+# =====================================================
+#
+# 'b' should remain uninstalled, but 'a' should be installed because 'b' is not
+# an absolute dependency of 'a'.
+#
+# Instead nothing is  installed.
+#
+# Status
+# ======
+# Open
+#
+
+import os
+import opk, cfg, opkgcl
+
+opk.regress_init()
+
+o = opk.OpkGroup()
+
+o.add(Package="x")
+o.add(Package="a", Recommends="b")
+o.add(Package="b", Conflicts="x")
+
+o.write_opk()
+o.write_list()
+
+opkgcl.update()
+
+opkgcl.install("x");
+if not opkgcl.is_installed("x"):
+    opk.fail("Package 'x' installed but reports as not installed")
+
+opkgcl.install("a");
+if not opkgcl.is_installed("x"):
+    opk.fail("Package 'x' removed when 'a' installed, but should not be removed.")
+if opkgcl.is_installed("b"):
+    opk.fail("Package 'b' installed but conflicts with x.")
+if not opkgcl.is_installed("a"):
+    opk.xfail("Package 'a' not installed even though 'b' is not an absolute dependency of 'a'.")
---
+++ tests/regress/issue165b.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python
+#
+# Reporter: Eric Yu
+#
+# What steps will reproduce the problem?
+# ======================================
+#
+# 1. Create packages 'x', 'a', 'b', and 'c' such that 'a' recommends 'b',
+# 'b' recommends 'c', and 'c' conflicts 'x'.
+# 2. Install 'x'.
+# 3. Try to install 'a'.
+#
+# What is the expected output? What do you see instead?
+# =====================================================
+#
+# 'a' and 'b' should be installed since 'c' is not an absolute dependency
+# of 'b'.
+#
+# Instead only 'b' is installed and 'a' remains uninstalled.
+#
+# Status
+# ======
+# Open
+#
+
+import os
+import opk, cfg, opkgcl
+
+opk.regress_init()
+
+o = opk.OpkGroup()
+
+o.add(Package="x")
+o.add(Package="a", Recommends="b")
+o.add(Package="b", Recommends="c")
+o.add(Package="c", Conflicts="x")
+
+o.write_opk()
+o.write_list()
+
+opkgcl.update()
+
+opkgcl.install("x");
+if not opkgcl.is_installed("x"):
+    opk.fail("Package 'x' installed but reports as not installed")
+
+opkgcl.install("a");
+if not opkgcl.is_installed("x"):
+    opk.fail("Package 'x' removed when 'a' installed, but should not be removed.")
+if not opkgcl.is_installed("b"):
+    opk.fail("Package 'b' not installed, but should be installed as a recommended pkg of 'a'.")
+if opkgcl.is_installed("c"):
+    opk.fail("Package 'c' installed but conflicts with x.")
+if not opkgcl.is_installed("a"):
+    opk.xfail("Package 'a' not installed even though 'c' is not an absolute dependency of 'b'.")
