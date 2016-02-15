982ad92217e7ba4101e0680ef42cc706c5d8c096
Alejandro del Castillo <alejandro.delcastillo@ni.com>
2016-02-15 11:11:59 -0600

    test/regress: add test for issue 9059
    Signed-off-by: Alejandro del Castillo <alejandro.delcastillo@ni.com>

---
+++ tests/regress/issue9059.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+#
+# Reporter: scot.salmon@ni.com
+#
+# What steps will reproduce the problem?
+# ======================================
+#
+# 1.- Create package 'a', which PROVIDES and CONFLICTS 'x'
+# 2.- Create package 'b', which DEPENDS on 'x'
+# 3.- Create package 'x'
+# 4.- Install 'a'
+# 5.- Install 'b'
+#
+# What is the expected output? What do you see instead?
+# ======================================
+#
+# Installation of b fails (it shouldn't)
+#
+
+import os
+import opk, opkgcl
+
+opk.regress_init()
+
+o = opk.OpkGroup()
+o.add(Package="a", Provides="x", Conflicts="x")
+o.add(Package="b", Depends="x")
+o.add(Package="x")
+o.write_opk()
+o.write_list()
+
+opkgcl.update()
+
+opkgcl.install("a")
+if not opkgcl.is_installed("a"):
+	opk.fail("Package 'a' installed but reports as not installed.")
+
+opkgcl.install("b")
+if not opkgcl.is_installed("b"):
+        opk.xfail("Package 'b' installed but reports as not installed.")
