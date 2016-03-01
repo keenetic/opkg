3e8521e137f662005edec1e7dd597bdd808624ea
Ross Burton <ross.burton@intel.com>
2016-03-01 18:03:40 +0000

    opkg_cmd: set force_reinstall when comparing versions
    If two versions compare the same then force_reinstall is used as a tie-breaker.
    However opkg_compare_versions_cmd doesn't set that so the value of the int is
    undefined, and the comparison will be random.
    
    Signed-off-by: Ross Burton <ross.burton@intel.com>

--- libopkg/opkg_cmd.c
+++ libopkg/opkg_cmd.c
@@ -1065,6 +1065,7 @@ static int opkg_compare_versions_cmd(int argc, char **argv)
         struct pkg p1, p2;
         parse_version(&p1, argv[0]);
         parse_version(&p2, argv[2]);
+        p1.force_reinstall = p2.force_reinstall = 0;
         return pkg_version_satisfied(&p1, &p2, argv[1]);
     } else {
         opkg_msg(ERROR,
