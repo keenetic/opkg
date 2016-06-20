a32abe64f25bda3aba9edcac8488940d705dc4f7
Michael Hansen <michael.j.hansen@hp.com>
2016-06-20 16:22:00 -0700

    opkg_execute_install: don't print Installing message when --download-only is used
    Regression from 0.3.1.
    
    Signed-off-by: Michael Hansen <michael.j.hansen@hp.com>
    Signed-off-by: Alejandro del Castillo <alejandro.delcastillo@ni.com>

--- libopkg/solvers/internal/opkg_install_internal.c
+++ libopkg/solvers/internal/opkg_install_internal.c
@@ -182,8 +182,10 @@ int opkg_execute_install(pkg_t *pkg, pkg_vec_t *pkgs_to_install, pkg_vec_t *repl
                         dependency->state_status == SS_UNPACKED)
             continue;
 
-        opkg_msg(NOTICE, "%s %s (%s) on %s.\n", from_upgrade ? "Upgrading" : "Installing",
-                 dependency->name, dependency->version, dependency->dest->name);
+        if (!opkg_config->download_only) {
+            opkg_msg(NOTICE, "%s %s (%s) on %s.\n", from_upgrade ? "Upgrading" : "Installing",
+                     dependency->name, dependency->version, dependency->dest->name);
+        }
 
         /* Set all pkgs to auto_installed except the top level */
         if (dependency != pkg)
