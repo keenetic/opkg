e5838be6bb3d0e4f3bdb083f1f8d9704fa61fd8c
Josh Cartwright <joshc@ni.com>
2016-07-19 13:52:30 -0500

    tests/core: add basic test for list-upgradable behavior
    Note: because currently libsolv doesn't implement list-upgradable, this
    test currently is an expected failure in libsolv builds.
    
    Signed-off-by: Josh Cartwright <joshc@ni.com>
    Signed-off-by: Alejandro del Castillo <alejandro.delcastillo@ni.com>

---
+++ tests/core/34_list_upgradable.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+#
+# Test opkg list-upgradable behavior.
+
+import os
+import opk, cfg, opkgcl
+
+opk.regress_init()
+
+o = opk.OpkGroup()
+o.add(Package="a", Version ="1.0")
+o.add(Package="a", Version ="2.0")
+o.add(Package="a", Version ="3.0")
+o.write_opk()
+o.write_list()
+
+opkgcl.update()
+
+opkgcl.install("a=1.0")
+if not opkgcl.is_installed("a", "1.0"):
+	opk.fail("Package 'a', Version '1.0', failed to install.")
+
+if opkgcl.is_upgradable("a", "2.0"):
+	opk.fail("Package 'a', is upgradable to version '2.0', though a better candidate is available.")
+
+if not opkgcl.is_upgradable("a", "3.0"):
+	opk.xfail("[libsolv] Package 'a', is not upgradable to version '3.0'.")
+
+opkgcl.install("a=2.0")
+if not opkgcl.is_installed("a", "2.0"):
+	opk.fail("Package 'a', Version '2.0', failed to install.")
+
+if not opkgcl.is_upgradable("a", "3.0"):
+	opk.xfail("[libsolv] Package 'a', is not upgradable to version '3.0'.")
+
+opkgcl.install("a=3.0")
+if not opkgcl.is_installed("a", "3.0"):
+	opk.fail("Package 'a', Version '3.0', failed to install.")
+
+if opkgcl.is_upgradable("a"):
+	opk.fail("Package 'a', is upgradable, but is already the latest.")
